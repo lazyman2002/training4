46a737b00c9b7643460f7a69e1659fe1dedf1c86
VoVietBac <bac24062002@gmail.com>
2024-09-18 14:43:20 +0700

    ?

--- bt4-1.sh
+++ bt4-1.sh
@@ -1,6 +1,5 @@
 bin/spark-submit \
-  # --master yarn \
+  --master local[*] \
   --class Main \
-  # --num-executors 4 \
-  # --jars $(echo /home/WBAC/hbase-2.6.0/lib/*.jar | tr ' ' ',') \
+  --jars $(echo /home/WBAC/hbase-2.6.0/lib/*.jar | tr ' ' ',') \
   training4/bt4-1_2.12-2.0.jar 8813685310712123492 2018-08-10
